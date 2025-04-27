bafba651277f79333272c66766180b390fea258b
Vinícius Ghilardi <viniciusgg98@gmail.com>
2025-04-27 05:56:08 -0300

    Criacao do modelo

--- manutencao/equipamentos/models.py
+++ manutencao/equipamentos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Equipamento(models.Model):
+    STATUS_CHOICES = [
+        ('verde', 'Verde - Funcionando'),
+        ('amarelo', 'Amarelo - Com defeito parcial'),
+        ('vermelho', 'Vermelho - Fora de uso'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=50)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='verde')
+    defeito_relatado = models.TextField(blank=True, null=True)
+    data_chamado = models.DateField(auto_now_add=True)
+    previsao_conserto = models.DateField(blank=True, null=True)
+    tecnico_responsavel = models.CharField(max_length=100, blank=True, null=True)
+    ultima_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
